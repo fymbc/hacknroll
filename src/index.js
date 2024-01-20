613203d1b9ba23f9b19e885947d4256512b354e2
Gallen <90502827+gongg21@users.noreply.github.com>
2024-01-21 00:29:40 +0800

    Update files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('helpdeskBtn').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {'message': 'helpdesk_request'}, function(response) {
-      console.log(response.message);
-    });
-  });
-});
