7210bc9f686340418f18149edb29935bf72d429c
Gallen <90502827+gongg21@users.noreply.github.com>
2024-01-20 16:58:12 +0800

    Update files

--- frontend/content.js
+++ frontend/content.js
@@ -3,28 +3,24 @@ function app() {
       const assistant = document.createElement('div');
       assistant.classList.add('assistant');
       document.body.appendChild(assistant);
-      
-    const speechBubble = document.createElement('div');
-    speechBubble.setAttribute('id', 'bubble');
-    speechBubble.classList.add('speech-bubble');
-    assistant.appendChild(speechBubble);
     
-    const textInSpeechBubble = document.createElement('h1');
-    textInSpeechBubble.textContent = 'How can I help?';
-    textInSpeechBubble.classList.add('popup');
-    speechBubble.appendChild(textInSpeechBubble);
-    
-    document.getElementById('bubble').style.display='none';
-
-    setTimeout(function () {
-      document.getElementById('bubble').style.display='block';
-    }, 3000);
-    
-
-  }
+        const speechBubble = document.createElement('div');
+        speechBubble.setAttribute('id', 'bubble');
+        speechBubble.classList.add('speech-bubble');
+        assistant.appendChild(speechBubble);
         
-window.addEventListener('load', init);
-
+        const textInSpeechBubble = document.createElement('h1');
+        textInSpeechBubble.textContent = 'How can I help?';
+        textInSpeechBubble.classList.add('popup');
+        speechBubble.appendChild(textInSpeechBubble);
+        
+        document.getElementById('bubble').style.display='none';
+        
+        setTimeout(function () {
+        document.getElementById('bubble').style.display='block';
+        }, 3000);
+    }
+    window.addEventListener('load', init);
 }
 
 let script = document.createElement('script');
